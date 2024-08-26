f39e28c492ab1e415ee460901ddc82fe6cd4ed5f
ilham hdytllh <ilham.hidayatullah18@gmail.com>
2024-08-26 09:12:21 +0700

    koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,30 +1,49 @@
 <?php
 date_default_timezone_set('Asia/Jakarta');
-$host="10.0.0.174";
-$username="ditprogram";
-$password="Xou@RUnivV!6";
-$db_name="TM";
-$connInfo = array( "Database"=>$db_name, "UID"=>$username, "PWD"=>$password);
-$conn     = sqlsrv_connect( $host, $connInfo);
-$con=mysqli_connect("10.0.0.10","dit","4dm1n","db_dying");
-$cond=mysqli_connect("10.0.0.10","dit","4dm1n","db_qc");
-$con_nowprd     = mysqli_connect("10.0.0.10","dit","4dm1n","nowprd");
-if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
-    }
 
-$hostname="10.0.0.21";
+$hostSVR19 = "10.0.0.221";
+$usernameSVR19 = "sa";
+$passwordSVR19 = "Ind@taichen2024";
+$dying = "db_dying";
+$qc = "db_qc";
+$nowprd = "nowprd_cache";
+$lab = "db_laborat";
+
+$db_dying = array("Database" => $dying, "UID" => $usernameSVR19, "PWD" => $passwordSVR19);
+$db_qc = array("Database" => $qc, "UID" => $usernameSVR19, "PWD" => $passwordSVR19);
+$nowprd = array("Database" => $nowprd, "UID" => $usernameSVR19, "PWD" => $passwordSVR19);
+$dbLab = array("Database" => $lab, "UID" => $usernameSVR19, "PWD" => $passwordSVR19);
+
+$con     = sqlsrv_connect($hostSVR19, $db_dying);
+$cond     = sqlsrv_connect($hostSVR19, $db_qc);
+$con_nowprd = sqlsrv_connect($hostSVR19, $nowprd);
+$conLab = sqlsrv_connect($hostSVR19, $dbLab);
+
+if ($con) {
+} else {
+    exit("SQLSVR19 Connection failed to con");
+}
+if ($cond) {
+} else {
+    exit("SQLSVR19 Connection failed to cond");
+}
+if ($con_nowprd) {
+} else {
+    exit("SQLSVR19 Connection failed to con_nowprd");
+}
+if ($conLab) {
+} else {
+    exit("SQLSVR19 Connection failed to conLab");
+}
+
+$hostname = "10.0.0.21";
 $database = "NOWPRD";
 $user = "db2admin";
 $passworddb2 = "Sunkam@24809";
-$port="25000";
+$port = "25000";
 $conn_string = "DRIVER={IBM ODBC DB2 DRIVER}; HOSTNAME=$hostname; PORT=$port; PROTOCOL=TCPIP; UID=$user; PWD=$passworddb2; DATABASE=$database;";
-$conn2 = db2_connect($conn_string,'', '');
-if($conn2) {
-}
-else{
+$conn2 = db2_connect($conn_string, '', '');
+if ($conn2) {
+} else {
     exit("DB2 Connection failed");
-    }
-
-
+}
