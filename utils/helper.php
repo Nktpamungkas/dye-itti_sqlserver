7d5a438cbd796f49117d4b0cffb0a55d758ac224
hadi.subhana <hadi.subhana@indotaichen.com>
2024-09-05 11:43:30 +0700

    update fungsi cek() pada helper

--- utils/helper.php
+++ utils/helper.php
@@ -1,25 +1,24 @@
 <?php
 
-function cek($value) {
-	if($value == NULL || $value == "") {
-		return NULL;
-	}
-  
-	if($value instanceof DateTime) {
-		if($value->format('Y-m-d') != '1900-01-01') {
-			return $value->format('Y-m-d');
-		} else {
-			return NULL;
-		}
-	}
-  
-	if($value == '1900-01-01') {
-		return NULL;
-	}
-  
-	if($value == '.00') {
-	  return NULL;
-	}
-  
-	return $value;
+function cek($value, $format = null) {
+    // Check for NULL or empty value
+    if (empty($value)) {
+        return NULL;
+    }
+
+    // Handle DateTime object
+    if ($value instanceof DateTime) {
+        if (is_null($format)) {
+            // Return 'Y-m-d' format if no specific format is provided and not equal to '1900-01-01'
+            return $value->format('Y-m-d') !== '1900-01-01' ? $value->format('Y-m-d') : NULL;
+        }
+        return $value->format($format);
+    }
+
+    // Handle specific string values
+    if ($value === '1900-01-01' || $value === '.00') {
+        return NULL;
+    }
+
+    return $value;
 }
