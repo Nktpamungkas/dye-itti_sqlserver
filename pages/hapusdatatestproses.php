61c394d02530fcbc001a37d32262414ce4807092
hadi.subhana <hadi.subhana@indotaichen.com>
2024-09-09 09:42:18 +0700

    DYEING > DATA TEST PROSES > LAPORAN DATA TEST PROSES. sudah selesai di migrasi kan ke sql server

--- pages/hapusdatatestproses.php
+++ pages/hapusdatatestproses.php
@@ -1,9 +1,7 @@
 <?php
-ini_set("error_reporting", 1);
-session_start();
-include("../koneksi.php");
+
     $modal_id=$_GET['id'];
-    $modal=mysqli_query($con,"DELETE FROM tbl_datatest WHERE id='$modal_id' ");
+    $modal=sqlsrv_query($con,"DELETE FROM db_dying.tbl_datatest WHERE id='$modal_id' ");
     if ($modal) {
         echo "<script>window.location='index1.php?p=Lap-DataTest-Proses';</script>";
     } else {
